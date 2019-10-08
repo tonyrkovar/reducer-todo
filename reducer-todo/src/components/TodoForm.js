6be91db52a3d4a6c32dac75e59ce1fed27c4c896
Tony <tonyrkovar@gmail.com>
2019-10-08 14:34:37 -0500

    Set up scaffolding, initialState and reducer

---
+++ reducer-todo/src/components/TodoForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const TodoForm = () => {
+    // handleInputs = e => {
+
+    // }
+
+    return (
+        <div>
+            <p>This will be the form</p>
+        </div>
+    )
+}
---
+++ reducer-todo/src/reducer/TodoReducer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const initialState = {
+    item: 'Test',
+    completed: false,
+    id: 3892987589
+}
+
+export const reducer = (state, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+
+}
